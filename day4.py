68a7b6b4bf107c9f6f78c6c3e8bdb7a9f5fa1cfc
Ysuri123 <121230600+Ysuri123@users.noreply.github.com>
2022-12-24 17:10:40 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,22 @@
+##def replace(n):
+##    n=n.replace('3','5')
+##    return n
+##inp=int(input())
+##inp=str(inp)
+##print(replace(inp))
+
+##def interchange3and5(n):
+##    n=str(n)
+##    n=n.replace('3','.')
+##    n=n.replace('5','3')
+##    n=n.replace('.','5')
+##    return n
+##inp=int(input())
+##print(interchange3and5(inp))
+
+
+def bintodecimal(n):
+    temp=int(n,2)
+    return temp
+inp=input()
+print(bintodecimal(inp))
